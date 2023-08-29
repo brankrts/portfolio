b09c47e12e73d12785cc9aabd351cd584ac851ad
Baran <brankrts@gmail.com>
2023-08-29 13:57:12 +0300

    home portfolio button link update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
           </p>
           <div>
             <Link
-              to="portfolio"
+              to="projects"
               smooth
               duration={500}
               className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer "
